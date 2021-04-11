53a3662d8c9591e02f64aaaa3a907b8dfd2ab4a2
Semerbl4 <69350287+Semerbl4@users.noreply.github.com>
2021-04-11 16:23:32 +0300

    Второй комит
    Добавлена библиотека classnames, добавлен функционал для модального окна.

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,22 +1,60 @@
 import './Modal.scss';
 
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+
+import cn from 'classnames';
 
 import { ModalContext } from '../../context';
 
-import { elementContextValue } from '../../App';
-
 export const Modal: React.FC = () => {
-  // interface IContext {
-  //   Component?: any; // пробовал и ReactNode и React.FC и React.Component и JSX.Element - ничего не работает.
-  // }
-  const { Component, type } = useContext(ModalContext) as typeof elementContextValue; // изначально тут не было as typeof elementContextValue
-  // и я пытался писать const { Component } : IContext, но всплывала ошибка
-  // Тип элемента JSX "Component" не имеет конструкций или сигнатур вызова.
-  return (
-    <div className="modal-container">
-      <Component />
-      <p>{type}</p>
-    </div>
-  );
+  const [isClosed, setIsClosed] = useState(false);
+
+  interface IContext {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    component?: any;
+    type?: string;
+    closeble?: boolean;
+    clickableBackground?: boolean;
+  }
+  const {
+    component: Component,
+    type = 'none',
+    closeble = true,
+    clickableBackground = true,
+  }: IContext = useContext(ModalContext);
+  useEffect(() => {
+    setIsClosed(false);
+  }, []);
+  if (!isClosed) {
+    return (
+      <div
+        className={cn({
+          'modal-container': true,
+          'modal-container_background-common': type === 'common',
+          'modal-container_background-error': type === 'error',
+          'modal-container_background-message': type === 'message',
+        })}
+        onClick={(): void => {
+          if (clickableBackground) {
+            setIsClosed(true);
+          }
+        }}
+      >
+        {closeble && (
+          <button
+            onClick={(): void => {
+              setIsClosed(true);
+            }}
+          >
+            Закрыть модалку
+          </button>
+        )}
+        <Component />
+        {console.log(closeble)}
+        {console.log(clickableBackground)}
+        <div>{type}</div>
+      </div>
+    );
+  }
+  return null;
 };
